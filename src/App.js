2221ac5a98c2ab7729b8c77dccc9f293a68fa890
sanakang0615 <sanakang0615@kaist.ac.kr>
2024-08-12 10:11:58 +0900

    Update the rendering order

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PinterestGrid from './components/PinterestGrid';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,26 +9,40 @@ import './index.css';
 
 function App() {
   const [currentItem, setCurrentItem] = useState(null);
+  const [isContentLoaded, setIsContentLoaded] = useState(false); // New state to control rendering
 
   const handleItemSelected = (item) => {
     setCurrentItem(item);
   };
+
   const handleClosePlayer = () => {
     setCurrentItem(null);
   };
 
+  // Use useEffect to delay the rendering of PinterestGrid
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsContentLoaded(true);
+    }, 1000); // Delay to ensure other components are loaded first
+
+    return () => clearTimeout(timer); // Clean up the timer on component unmount
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Header artworks={items} items={items} />
-      <ProfileHeader/>
+      <ProfileHeader />
       <section id="gallery" className="pb-16 px-6">
-      <h2 
-        className="text-4xl font-bold text-gray-600 text-center mb-10 grey-qo-regular" 
-        style={{ fontWeight: 500, color: '#4B5563', opacity: '0.4' }} // Matching the color with the icon
-      >
-        Track List
-      </h2>
-        <PinterestGrid items={items} onItemSelected={handleItemSelected} />
+        <h2 
+          className="text-4xl font-bold text-gray-600 text-center mb-10 grey-qo-regular" 
+          style={{ fontWeight: 500, color: '#4B5563', opacity: '0.4' }}
+        >
+          Track List
+        </h2>
+        {/* Conditionally render PinterestGrid only after other components are loaded */}
+        {isContentLoaded && (
+          <PinterestGrid items={items} onItemSelected={handleItemSelected} />
+        )}
       </section>
       {currentItem && (
         <MusicPlayer
@@ -36,7 +50,6 @@ function App() {
           currentItem={currentItem}
           key={currentItem.src}
           onClose={handleClosePlayer}
-
         />
       )}
       <Footer />
